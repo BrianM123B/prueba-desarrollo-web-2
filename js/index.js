3d4ac8ff68f2be50f739f38f2fc1611e9bcb7b4f
BrianM123B <brianmorenoluna@gmail.com>
2025-09-12 20:09:44 -0300

    Agregnado desafio 2

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// let edad = prompt("ingresa tu EDAD");
+// alert("Hola" + edad);
+// edad = edad +10;
+
+
+// clase 2
+// Ctrl + }
+// console.log(edad);
+const nombreProducto = "guitarra"
+let CantidadDeseada = prompt("Ingrese La cantidad deseada de " + nombreProducto);
+const precioUnitario = 1000000;
+costoTotal = CantidadDeseada * precioUnitario;
+
+alert("el costo total es: " + costoTotal + " dolares por " + CantidadDeseada + " De " + nombreProducto );
