6f0ee884a6cfda8d96186906b3729a98d51d1fec
BrianM123B <brianmorenoluna@gmail.com>
2025-09-19 19:52:35 -0300

    agregue desafio N3

--- js/index.js
+++ js/index.js
@@ -6,9 +6,16 @@
 // clase 2
 // Ctrl + }
 // console.log(edad);
+
+
 const nombreProducto = "guitarra"
-let CantidadDeseada = prompt("Ingrese La cantidad deseada de " + nombreProducto);
-const precioUnitario = 1000000;
-costoTotal = CantidadDeseada * precioUnitario;
+let CantidadDeseada = parseInt(prompt("Ingrese La cantidad deseada de " + nombreProducto));
+const precioUnitario = 1000;
+let costoTotal = CantidadDeseada * precioUnitario;
+
+if (CantidadDeseada >= 5) {
+    costoTotal = CantidadDeseada * precioUnitario * 0.90;
+}
 
 alert("el costo total es: " + costoTotal + " dolares por " + CantidadDeseada + " De " + nombreProducto );
+
