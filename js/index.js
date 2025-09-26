81e82adc3b334a52a451bf48816e8f9ac32b7b26
BrianM123B <brianmorenoluna@gmail.com>
2025-09-26 20:10:28 -0300

    agregando desafio 4

--- js/index.js
+++ js/index.js
@@ -9,13 +9,33 @@
 
 
 const nombreProducto = "guitarra"
-let CantidadDeseada = parseInt(prompt("Ingrese La cantidad deseada de " + nombreProducto));
+const CantidadDeseada = parseInt(prompt("Ingrese La cantidad deseada de " + nombreProducto));
 const precioUnitario = 1000;
-let costoTotal = CantidadDeseada * precioUnitario;
+// let costoTotal = CantidadDeseada * precioUnitario;
 
-if (CantidadDeseada >= 5) {
+
+
+// alert("el costo total es: " + costoTotal + " dolares por " + CantidadDeseada + " De " + nombreProducto );
+
+
+// let nombre = prompt ("Ingresa tu nombre: ")
+
+
+// function saludar () {
+//     alert("Holaa!!")
+// }
+
+// saludar()
+
+function sumarProductos(precioUnitario, CantidadDeseada) {
+    let costoTotal = precioUnitario*CantidadDeseada;
+      if (CantidadDeseada >= 5) {
     costoTotal = CantidadDeseada * precioUnitario * 0.90;
 }
+    alert("El total es: " + costoTotal)
+  
+}
 
-alert("el costo total es: " + costoTotal + " dolares por " + CantidadDeseada + " De " + nombreProducto );
-
+// sumarProductos(1000, 1)
+sumarProductos(precioUnitario,CantidadDeseada)
+console.log(sumarProductos)
